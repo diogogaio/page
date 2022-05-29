1ef33e52391f4d597848e5b1737c7528d75cc260
diogogaio <diogogaio@gmail.com>
2022-05-29 13:51:02 -0300

    padronização
    resolução de bug
    updates
    links

--- agenda.js
+++ agenda.js
@@ -1,11 +1,9 @@
 'use strict'
-//implement contact menu with edit contact and delete contact
-// build edit contact page
+
 // are you sure you want to delete contact confirmation
 // add confirmation boxes after edited and saved buttons clicked
 //make span reads if celular or home for exemple
 //format phone numbers to a standard display type
-//retur to proper page after creating contact
 
 //create class opacity 0 on all except page 1
 // create class fade-in max opacity, transition 0.3s, max z-index 
@@ -37,6 +35,7 @@ const fnameEdit = document.getElementById('fname-input-four');
 const lnameEdit = document.getElementById('lname-input-four');
 const phoneEdit = document.getElementById('phone-number-four');
 const savedPopUp = document.getElementById('saved-contact');
+const deletedPopUp = document.getElementById('deleted-contact');
 const magnifyIcon = document.querySelector('.fa-magnifying-glass');
 
 /* NAVIGATE */ const Navigate = {
@@ -90,31 +89,36 @@ backBtnFour.addEventListener('click', Navigate.backBtnFour)
     saved() {
         savedPopUp.style.display = "block";
         setTimeout(() => savedPopUp.style.display = "none", 2000)
+    },
+
+    deleted() {
+        deletedPopUp.style.display = "block";
+        setTimeout(() => deletedPopUp.style.display = "none", 2000)
     }
 }
 
 /* STORAGE */ const Storage = {
 
     getDataBase() {
-        return JSON.parse(localStorage.getItem('dataBase')) ?? []; ///no exemplo do fernando leonid (task list) não precisou usar 'return' function
+        return JSON.parse(localStorage.getItem('contactsDb')) ?? []; ///no exemplo do fernando leonid (task list) não precisou usar 'return' function
     }
 }
-const dataBase = Storage.getDataBase();
-console.log(dataBase);
+const contactsDb = Storage.getDataBase();
+console.log(contactsDb);
 
 /* SAVING */ const Saving = {
 
-    setDataBase(dataBase) {
+    setContactDataB(contactsDb) {
 
-        localStorage.setItem('dataBase', JSON.stringify(dataBase));
+        localStorage.setItem('contactsDb', JSON.stringify(contactsDb));
     },
 
     submit() {
         const contactData = { "fname": nameInput.value, "lname": lastName.value, "phoneNumber": phoneNumber.value };
 
-        dataBase.push(contactData);
-        Saving.setDataBase(dataBase);
-        console.log(dataBase)
+        contactsDb.push(contactData);
+        Saving.setContactDataB(contactsDb);
+        console.log(contactsDb)
 
         Display.contactList();
         ContactPage.pageContent();
@@ -134,7 +138,7 @@ saveBtn.addEventListener('click', Saving.submit)
 
         ul.innerHTML = "";
 
-        dataBase.forEach((data, index) => {
+        contactsDb.forEach((data, index) => {
 
             let contact = document.createElement('li');
             contact.setAttribute('data-index', index)
@@ -151,14 +155,14 @@ saveBtn.addEventListener('click', Saving.submit)
         console.log(searchTerm)
         ul.innerHTML = "";
 
-        for (let i = 0; i < dataBase.length; i++) { //get the index of info?
+        for (let i = 0; i < contactsDb.length; i++) { //get the index of info?
 
-            if (dataBase[i].fname.toLowerCase().startsWith(searchTerm)) {
+            if (contactsDb[i].fname.toLowerCase().startsWith(searchTerm)) {
 
-                console.log(dataBase[i].fname)
+                console.log(contactsDb[i].fname)
                 let contact = document.createElement('li');
                 contact.setAttribute('data-index', i)
-                contact.textContent = `${dataBase[i].fname} ${dataBase[i].lname}`
+                contact.textContent = `${contactsDb[i].fname} ${contactsDb[i].lname}`
                 ul.appendChild(contact);
                 console.log(contact);
             }
@@ -166,17 +170,20 @@ saveBtn.addEventListener('click', Saving.submit)
 
     },
 
+    showIcon() {
+        if (searchBox.value.length === 0) {
+            magnifyIcon.style.display = "block";
+        }
+    },
+
     clearSearch() {
         searchBox.value = '';
         Display.contactList();
+        Display.showIcon();
     },
 
     hideIcon() {
         magnifyIcon.style.display = "none";
-    },
-
-    showIcon() {
-        magnifyIcon.style.display = "block";
     }
 }
 Display.contactList();
@@ -190,11 +197,12 @@ clearBtn.addEventListener('click', Display.clearSearch);
     deleteContact() {
         let index = contactId.dataset.index;
         console.log(index)
-        dataBase.splice(index, 1);
-        Saving.setDataBase(dataBase); //if you dont pass database argument, localStorage value will become undentified
+        contactsDb.splice(index, 1);
+        PopUps.deleted();
+        Saving.setContactDataB(contactsDb); //if you dont pass contactsDb argument, localStorage value will become undentified
         Display.contactList();
         Navigate.showPageOne();
-        console.log(dataBase);
+        console.log(contactsDb);
     },
 
     pageContent(e) {
@@ -203,14 +211,14 @@ clearBtn.addEventListener('click', Display.clearSearch);
             Navigate.showPageThree();
             let index = e.target.dataset.index;
             contactId.setAttribute('data-index', index); //CONTACTID IS A DIV CREATED JUST TO STORE CONTACT INDEX IN ARRAY TO BE MANIPULATED LATER ON IF NEEDED -->
-            contactName.textContent = `${dataBase[index].fname} ${dataBase[index].lname}`;
-            contactNumber.textContent = `${dataBase[index].phoneNumber}`;
+            contactName.textContent = `${contactsDb[index].fname} ${contactsDb[index].lname}`;
+            contactNumber.textContent = `${contactsDb[index].phoneNumber}`;
         } else {
-            const editedContactIndex = dataBase.length - 1;
+            const editedContactIndex = contactsDb.length - 1;
             console.log(editedContactIndex);
             contactId.setAttribute('data-index', editedContactIndex);
-            contactName.textContent = `${dataBase[editedContactIndex].fname} ${dataBase[editedContactIndex].lname}`;
-            contactNumber.textContent = `${dataBase[editedContactIndex].phoneNumber}`;
+            contactName.textContent = `${contactsDb[editedContactIndex].fname} ${contactsDb[editedContactIndex].lname}`;
+            contactNumber.textContent = `${contactsDb[editedContactIndex].phoneNumber}`;
         }
     },
 };
@@ -226,23 +234,23 @@ deleteBtn.addEventListener('click', ContactPage.deleteContact);
 
     editPageContent() {
         Navigate.showPageFour();
-        fnameEdit.value = dataBase[EditPage.contactIndex()].fname
-        lnameEdit.value = dataBase[EditPage.contactIndex()].lname
-        phoneEdit.value = dataBase[EditPage.contactIndex()].phoneNumber
+        fnameEdit.value = contactsDb[EditPage.contactIndex()].fname
+        lnameEdit.value = contactsDb[EditPage.contactIndex()].lname
+        phoneEdit.value = contactsDb[EditPage.contactIndex()].phoneNumber
     },
 
     saveEditedContact() {
 
         const contactData = { "fname": fnameEdit.value, "lname": lnameEdit.value, "phoneNumber": phoneEdit.value };
 
-        dataBase.push(contactData);
-        dataBase.splice(EditPage.contactIndex(), 1);
-        Saving.setDataBase(dataBase);
+        contactsDb.push(contactData);
+        contactsDb.splice(EditPage.contactIndex(), 1);
+        Saving.setContactDataB(contactsDb);
         Display.contactList();
         ContactPage.pageContent();
         PopUps.saved();
         Navigate.showPageThree();
-        console.log(dataBase)
+        console.log(contactsDb)
     }
 }
 saveBtnFour.addEventListener('click', EditPage.saveEditedContact);
